12ef0ff9231dfb6a9908fbaa1aac974839ca23d0
Parth Sahoo <iamparrth@gmail.com>
2025-10-19 23:36:59 +0530

    test

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const { google } = require("googleapis");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(
+  cors({
+    origin: "https://www.tdwmarine.com",
+  })
+);
+
 app.get("/deadline", async (req, res) => {
   const email = req.query.email;
   if (!email) return res.status(400).json({ error: "Missing email parameter" });
